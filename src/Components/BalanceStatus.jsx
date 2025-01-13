785729c3a45df142d4ca3e304ac7e99dfac8be46
Nafisa Mila <jabinmila@gmail.com>
2025-01-13 15:08:31 +0600

    Updated sort, filter, balance

--- src/Components/BalanceStatus.jsx
+++ src/Components/BalanceStatus.jsx
@@ -8,15 +8,24 @@ export default function BalanceStatus({ balance, income, expense }) {
                     <dl className="grid grid-cols-1 text-center lg:grid-cols-3 divide-x-2 border rounded-md overflow-hidden">
                         <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
                             <dt className="text-base leading-7 text-gray-600">Balance</dt>
-                            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">BDT {balance}</dd>
+                            <dd
+                                className={`order-first text-xl font-semibold tracking-tight sm:text-3xl 
+                                ${balance < 0 ? 'text-red-600' : 'text-gray-700'}`}
+                            >
+                                BDT {balance}
+                            </dd>
                         </div>
                         <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
                             <dt className="text-base leading-7 text-gray-600">Total Income</dt>
-                            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">BDT {income}</dd>
+                            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+                                BDT {income}
+                            </dd>
                         </div>
                         <div className="bg-[#F9FAFB] flex lg:max-w-xs flex-col px-4 py-4">
                             <dt className="text-base leading-7 text-gray-600">Total Expense</dt>
-                            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">BDT {expense}</dd>
+                            <dd className="order-first text-xl font-semibold tracking-tight text-gray-700 sm:text-3xl">
+                                BDT {expense}
+                            </dd>
                         </div>
                     </dl>
                 </div>
